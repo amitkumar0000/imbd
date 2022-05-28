909eaf2535cbd77d68bc7f580a910956119577c6
amitkumar00000 <amit.ap1924@gmail.com>
2022-05-28 16:04:01 +0530

    added apikey

--- imdb-core/src/main/java/com/movie/imdbcore/source/MovieApiService.kt
+++ imdb-core/src/main/java/com/movie/imdbcore/source/MovieApiService.kt
@@ -3,6 +3,7 @@ package com.movie.imdbcore.source
 import com.movie.imdbcore.model.Movies
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface MovieApiService {
     /**
@@ -10,6 +11,7 @@ interface MovieApiService {
      *
      * @return [Single]
      */
+    @Headers("api-key: 38a73d59546aa378980a88b645f487fc")
     @GET("/t/p/w500/p60VSQL7usdxztIGokJPpHmKWdU.jpg")
     fun getMovieList(): Single<Movies>
 
